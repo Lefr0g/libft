a971158b603533de42dd5ff67e980c0564bbaf79
Lefr0g <lefr0g@lefr0g-1025C>
2014-11-09 22:53:12 +0100

    ft_strcat fonctionnel. strcpy pour remplir les chaines malloc

--- ft_strcat.c
+++ ft_strcat.c
@@ -11,7 +11,6 @@
 /* ************************************************************************** */
 //
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strcat(char *dest, const char *src)
 {
@@ -20,33 +19,13 @@ char	*ft_strcat(char *dest, const char *src)
 
 	i = 0;
 	origin = 0;
-	printf("<<< DEBUG >>> ft_strcat : Arrivee devant la boucle 1");
 	while (dest[origin] != '\0')
-	{
-		printf("<<< DEBUG >>> ft_strcat : Boucle 1, iteration %d", origin);
 		origin++;
-	}
 	while (src[i] != '\0')
 	{
-		printf("<<< DEBUG >>> ft_strcat : Boucle 2, iteration %d", i);
 		dest[origin + i] = src[i];
 		i++;
 	}
 	dest[origin + i] = '\0';
 	return (dest);
-}
-
-int	main(void)
-{
-	char *source;
-	char *destination;
-
-	printf("<<< DEBUG >>> main : Debut des Mallocs"); 
-	source = (char*)malloc(sizeof(char) * 42);
-	destination = (char*)malloc(sizeof(char) * 42);
-	printf("<<< DEBUG >>> main : Fin des Mallocs"); 
-	source = "ABCDE";
-	destination = "01234";
-	destination = ft_strcat(destination, source);
-	return (0);
 }
