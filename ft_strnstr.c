e6b5a3a4bad246ed5b00f63f12783b57b6525a93
Antoine MULIN <antoine.mulin@gmail.com>
2014-11-19 18:04:17 +0100

    Optimisation

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/14 12:42:48 by amulin            #+#    #+#             */
-/*   Updated: 2014/11/19 17:21:05 by amulin           ###   ########.fr       */
+/*   Updated: 2014/11/19 18:00:49 by amulin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 	j = 0;
 	if (ft_strlen(needle) > n)
 		return (NULL);
+	if (needle[j] == '\0' || j == n)
+		return ((char*)&haystack[i - j]);
 	while (haystack[i] != '\0')
 	{
-		if (needle[j] == '\0' || j == n)
-			return ((char*)&haystack[i - j]);
 		if (needle[j] == haystack[i])
 			j++;
 		i++;
--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/14 12:42:59 by amulin            #+#    #+#             */
-/*   Updated: 2014/11/19 16:38:04 by amulin           ###   ########.fr       */
+/*   Updated: 2014/11/19 18:01:27 by amulin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ char *ft_strstr(const char *haystack, const char *needle)
 	j = 0;
 	if (haystack[i] == needle[j] && haystack[i] == 0)
 		return ("");
+	if (needle[j] == '\0')
+		return ((char*)&haystack[i - j]);
 	while (haystack[i] != '\0')
 	{
-		if (needle[j] == '\0')
-			return ((char*)&haystack[i - j]);
 		if (haystack[i] == needle[j])
 			j++;
 		i++;
