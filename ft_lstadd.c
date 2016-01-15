c422648a05982061216709024c5611f4b4fda3cc
Antoine MULIN <amulin@e3r4p5.42.fr>
2016-01-15 16:37:56 +0100

    Double-linked list now working with lstadd() and lstappend()

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,7 +6,7 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/08 16:23:04 by amulin            #+#    #+#             */
-/*   Updated: 2014/12/08 17:54:05 by amulin           ###   ########.fr       */
+/*   Updated: 2016/01/15 16:21:49 by amulin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ void	ft_lstadd(t_list **alst, t_list *new)
 	ptr = *alst;
 	if (alst && *alst && new)
 	{
-		while (ptr->next)
-			ptr = ptr->next;
+		ptr->prev = new;
 		new->next = *alst;
 		new->prev = NULL;
 		*alst = new;
