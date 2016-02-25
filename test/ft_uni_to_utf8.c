dbc8348d6fdcfa6277ce48a3aea125450eed4c84
amulin <amulin@student.42.fr>
2016-02-25 19:58:40 +0100

    Uni to UTF8 now working. WIP : compressing code for norme

--- test/ft_uni_to_utf8.c
+++ test/ft_uni_to_utf8.c
@@ -6,7 +6,7 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/25 14:59:39 by amulin            #+#    #+#             */
-/*   Updated: 2016/02/25 18:56:33 by amulin           ###   ########.fr       */
+/*   Updated: 2016/02/25 19:57:29 by amulin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,47 @@
 
 unsigned int	ft_uni_to_utf8(wchar_t codepoint)
 {
-	unsigned char			in[4];
-	unsigned char	lead_byte_2;
-	unsigned char	lead_byte_3;
-	unsigned char	lead_byte_4;
+	unsigned char	lead_byte;
 	unsigned char	cont_byte;
 	unsigned int	output;
 	unsigned char	buf;
-	unsigned int	buf2;
 
-	lead_byte_2 = 0xC0;
-	lead_byte_3 = 0xE0;
-	lead_byte_4 = 0xF0;
 	cont_byte = 0x80;
 	buf = 0;
-	buf2 = 0;
-
-	ft_memcpy(in, &codepoint, 4);
-/*
-	ft_putnbr(in[0]);
-	ft_putchar('\n');
-	ft_putnbr(in[1]);
-	ft_putchar('\n');
-	ft_putnbr(in[2]);
-	ft_putchar('\n');
-	ft_putnbr(in[3]);
-	ft_putchar('\n');
-*/
+	output = 0;
 	if (codepoint <= 0x007F)
-	{
 		output = (unsigned int)codepoint;
-	}
-	else if (codepoint <= 0x07FF)
+	else if (codepoint <= 0x1FFFFF)
 	{
-		buf = (unsigned char)codepoint;
-		buf = (unsigned char) (buf << 2) >> 2;
+		buf = (unsigned char) (codepoint << 2) >> 2;
 		output = (buf | cont_byte) << 8;
-
-		buf = (unsigned char) codepoint >> 6;
-		buf |= lead_byte_2;
-		output |= buf;
+		if (codepoint <= 0x7FF)
+			output |= (codepoint >> 6) | 0xC0;
+		else
+		{
+			buf = (unsigned char) codepoint >> 6;
+			output = output << 8;
+			output |= (buf | cont_byte) << 8;
+			if (codepoint <= 0xFFFF)
+			{
+				lead_byte = 0xE0;
+				buf = codepoint >> 12;
+				buf |= lead_byte;
+				output |= buf;
+			}
+			else
+			{
+				lead_byte = 0xF0;
+				buf = codepoint >> 12;
+				buf = (unsigned char) (buf << 2) >> 2;
+				output = output << 8;
+				output |= (buf | cont_byte) << 8;
+				buf = codepoint >> 20;
+				buf |= lead_byte;
+				output |= buf;
+			}
+		}
 	}
-	else
-		return (0);
 	return (output);
 }
 
@@ -64,31 +62,27 @@ unsigned int	ft_uni_to_utf8(wchar_t codepoint)
 #include <wchar.h>
 #include <locale.h>
 
-int	main(void)
+void	compare(wchar_t unicode)
 {
-	wchar_t			unicode;
 	unsigned int	utf8;
 
-	setlocale(LC_ALL, "");
-
-	unicode = 0xE9;
-
 	ft_putstr("Unicode = ");
-	write(1, &unicode, 4);
-	ft_putchar('\n');
-	ft_print_memory(&unicode, 4);
-
+	ft_puthex(unicode, "min");
 	utf8 = ft_uni_to_utf8(unicode);
-
 	ft_putstr("\nUTF-8 = ");
-//	utf8 = 0xC3A9;
 	write(1, &utf8, 4);
-	ft_putchar('\n');
+	printf("\nWith printf : %lc\n", unicode);
 	ft_print_memory(&utf8, 4);
 	ft_putchar('\n');
-	printf("Before\n");
-	printf("With printf : %lc\n", unicode);
-	printf("After\n");
+}
 
+int	main(void)
+{
+	setlocale(LC_ALL, "");
+
+	compare(0xE9);
+	compare(0x7F7);
+	compare(0x20AC);
+	compare(0x20012);
 	return (0);
 }
