4238271b834771d243a24a3052fa8025e83d37e1
amulin <amulin@student.42.fr>
2016-04-07 20:32:26 +0200

    Quick add to an header file. Previous TODO is still relevant, plus check all header files to take advantage of new Makefile

--- includes/errmgmt.h
+++ includes/errmgmt.h
@@ -6,7 +6,7 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/07 14:34:24 by amulin            #+#    #+#             */
-/*   Updated: 2016/04/07 14:35:01 by amulin           ###   ########.fr       */
+/*   Updated: 2016/04/07 20:22:56 by amulin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 # include "libft.h"
 # include <errno.h>
+
+void	ft_print_error(char *progname, char *arg, int errnum);
 
 #endif
