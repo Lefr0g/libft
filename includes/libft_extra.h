cc340c5b930d69a243c91ac6ebd92503325007f9
Lefr0g <amulin@student.42.fr>
2016-06-16 20:40:09 +0200

    Reorganized libft_extra.h header

--- includes/libft_extra.h
+++ includes/libft_extra.h
@@ -22,20 +22,51 @@
 # include "libft.h"
 
 /*
-**	Mine
+**	Convenience algorithms
 */
+int					get_next_line(int const fd, char **line);
+char				ft_parse_options(char **tested, char **valid,
+		char ***checked);
+
+
+/*
+**	Simple math & variable value comparison
+*/
+int					ft_getmax(int a, int b);
+int					ft_getmin(int a, int b);
+
 int					ft_abs(int i);
 long long			ft_abs_ll(long long i);
 
 int					ft_tabmax(int *tab, int siz);
 int					ft_tabmin(int *tab, int siz);
 
+
+/*
+**	Binary
+**	(made for ft_printf)
+*/
 unsigned int		ft_endian_swap(unsigned int input);
-unsigned int		ft_uni_to_utf8(wchar_t codepoint);
 int					ft_putbin(unsigned long long n, int sizeof_type);
 
+
+/*
+**	Hexa
+**	(made for ft_printf)
+*/
+int					ft_ishexa(int c);
+int					ft_hexchartoi(char c);
+int					ft_hexbytetoi(char *hexbyte);
+void				ft_puthex(unsigned int n, char *mode);
+
+
+/*
+**	Unicode / UTF-8
+**	(made for ft_printf)
+*/
 int					ft_putwchar(wchar_t c);
 int					ft_putwstr(wchar_t const *wstr);
+unsigned int		ft_uni_to_utf8(wchar_t codepoint);
 size_t				ft_wchar_utf8len(wchar_t const wchar);
 size_t				ft_wstr_utf8len(wchar_t const *wstr);
 size_t				ft_wstr_memsize(wchar_t const *wstr);
@@ -43,13 +74,21 @@ size_t				ft_wcslen(const wchar_t *s);
 wchar_t				*ft_wcsncpy(wchar_t *dst, const wchar_t *src, size_t n);
 wchar_t				*ft_utf8ncpy(wchar_t *dst, const wchar_t *src, size_t n);
 
-int					ft_ishexa(int c);
-int					ft_hexchartoi(char c);
-int					ft_hexbytetoi(char *hexbyte);
+/*
+**	Other ft_printf
+*/
+char				*ft_itoa_ll(long long int n, unsigned int base);
+char				*ft_itoa_ull(unsigned long long int n, unsigned int base);
+void				ft_putoctal(unsigned long long n);
+void				ft_puthex_ull(unsigned long long int n, char *made);
+void				ft_putnbr_ull(unsigned long long int n);
+void				ft_putnbr_ll(long long int n);
 
+/*
+**	Debug
+*/
 void				ft_print_memory(const void *addr, size_t size);
-void				ft_puthex(unsigned int n, char *mode);
-int					get_next_line(int const fd, char **line);
+
 
 /*
 **	Lists
@@ -59,25 +98,12 @@ t_list				*ft_lstinsertright(t_list *elem, t_list *pos);
 int					ft_lstsort(t_list **alst, int content_offset,
 		int (*compare)(void*, void*));
 
-/*
-**	Made for ft_printf
-*/
-int					ft_getmax(int a, int b);
-int					ft_getmin(int a, int b);
-void				ft_puthex_ull(unsigned long long int n, char *mode);
-void				ft_putoctal(unsigned long long n);
-void				ft_putnbr_ull(unsigned long long int n);
-void				ft_putnbr_ll(long long n);
-char				*ft_itoa_ll(long long int n, unsigned int base);
-char				*ft_itoa_ull(unsigned long long n, unsigned int base);
 
 /*
-** CLI management
+**	Str arrays
+**	(used for ft_parse_options)
 */
-char				ft_parse_options(char **tested, char **valid,
-		char ***checked);
 char				*ft_find_double_str(char *needle, char **haystack);
-
 char				**ft_strarray_dup(char **src);
 void				ft_strarray_del(char ***array);
 
