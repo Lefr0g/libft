e25365fdd87941c5881f6b0be2ce36e7dc518dcd
Lefr0g <amulin@student.42.fr>
2016-09-13 18:22:33 +0200

    Bug found in ft_strsplit.c. New implementation done

--- sources/vanilla/ft_strsplit.c
+++ sources/vanilla/ft_strsplit.c
@@ -1,95 +1,61 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strsplit.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/12/01 20:10:47 by amulin            #+#    #+#             */
-/*   Updated: 2015/02/13 16:25:29 by amulin           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
-static	void	my_varsetzero(size_t *i)
+static int	count_elems(char const *s, char c)
 {
-	i[1] = 0;
-	i[2] = 0;
-	i[3] = 0;
-}
-
-static size_t	my_counter(char const *s, char c)
-{
-	size_t	i;
-	size_t	count;
-	size_t	flag;
+	int	i;
+	int	count;
 
 	i = 0;
 	count = 0;
-	flag = 0;
-	while (s[i] != '\0')
+	while (s[i])
 	{
-		if (s[i] == c && s[i + 1] != c && s[i + 1] != '\0')
-			count++;
 		if (s[i] != c)
-			flag++;
-		i++;
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+		if (s[i] && s[i] == c)
+			while (s[i] && s[i] == c)
+				i++;
 	}
-	if (i != 0 && count == 0 && flag != 0)
-		count++;
 	return (count);
 }
 
-static size_t	my_strlenchar(char const *s, char c, size_t *i)
+static int	strlentochar(char const *s, char c)
 {
-	size_t	x;
-	size_t	len;
+	int	pos;
 
-	len = 0;
-	x = *i;
-	if (s == NULL)
-		return (0);
-	while (s[x] == c)
-		x++;
-	while (s[x] != '\0' && s[x] != c)
-	{
-		len++;
-		x++;
-	}
-	*i = x;
-	return (len);
+	pos = 0;
+	while (s[pos] && s[pos] != c)
+		pos++;
+	return (pos);
 }
 
-static void		my_strfill(char const *s, char **buff, size_t *i)
+char		**ft_strsplit(char const *s, char c)
 {
-	ft_strncpy(buff[i[3]], &s[i[1] - i[2]], i[2]);
-	buff[i[3]][i[2]] = '\0';
-}
+	char	**tab;
+	int		i;
+	int		word;
+	int		count;
 
-char			**ft_strsplit(char const *s, char c)
-{
-	char	**buff;
-	size_t	i[4];
-
-	if (s == NULL)
+	count = count_elems(s, c);
+	if (!(tab = (char**)malloc(sizeof(char*) * count + 1)))
 		return (NULL);
-	i[0] = my_counter(s, c);
-	buff = (char**)malloc(sizeof(char*) * (i[0] + 1));
-	if (buff == NULL)
-		return (NULL);
-	my_varsetzero(i);
-	while (i[3] <= i[0])
+	i = 0;
+	word = 0;
+	while (s[i])
 	{
-		i[2] = my_strlenchar(s, c, &i[1]);
-		buff[i[3]] = ft_strnew(i[2]);
-		if (buff[i[3]] == NULL)
-			return (NULL);
-		if (i[3] == i[0])
-			buff[i[3]] = NULL;
-		else
-			my_strfill(s, buff, i);
-		i[3]++;
+		if (s[i] != c)
+		{
+			count = strlentochar(&s[i], c);
+			if (!(tab[word] = ft_strnew(count + 1)))
+				return (NULL);
+			ft_strncpy(tab[word], &s[i], count);
+			word++;
+		}
+		i = s[i] != c ? i + count : i + 1;
 	}
-	return (buff);
+	tab[word] = NULL;
+	return (tab);
 }
